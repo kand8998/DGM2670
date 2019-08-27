6aa07ebbbb4ddff41dcdd0f3c07ef54d448cfef3
Kaitlyn <kand8998@gmail.com>
2019-08-27 11:32:49 -0600

    Intro to Awake Function

---
+++ FallScripting2019/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    // Unity Tutorial Variant
+
+    private void Awake()
+        //References between scripts when game starts
+    {
+        Debug.Log("I'm Awake");
+    }
+
+    private void Start()
+        //Begins when made active, only happens once
+    {
+        Debug.Log("Time to Start the Day!");
+    }
+
+
+}
