64eeaf6a6d1a4fe48f70da2e789a1a7d3f9eca5a
Kaitlyn <kand8998@gmail.com>
2019-08-27 11:16:59 -0600

    Loops Refresher
    Unity Tutorial Variant, as well as Unity Project adapted to Unity 2018 3.14

---
+++ FallScripting2019/Assets/Scripts/Loops.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Loops : MonoBehaviour
+{
+    // Unity Tutorial Variant and Testing
+
+    private int eggsInTheBasket = 12;
+    public UnityEvent ContinueEvent;
+    private int numEnemies = 3;
+
+    private void Start()
+    {
+        while (eggsInTheBasket > 0)
+        {
+            Debug.Log("We have eggs!");
+            eggsInTheBasket--;
+        }
+
+        bool shouldContinue = false;
+
+        do
+        {
+            ContinueEvent.Invoke();
+        } while (shouldContinue == true);
+
+        for (int i = 0; i < numEnemies; i++)
+        {
+            Debug.Log("Creating enemy number: " + i);
+        }
+    }
+    
+}
