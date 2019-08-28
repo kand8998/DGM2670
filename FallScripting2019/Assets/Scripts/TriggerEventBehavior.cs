03a0462538b35881bef74f8c06e5d921a33d6e46
Kaitlyn <kand8998@gmail.com>
2019-08-28 13:46:42 -0600

    Float Data and Trigger Events

---
+++ FallScripting2019/Assets/Scripts/TriggerEventBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehavior : MonoBehaviour
+{
+   public UnityEvent triggerEnterEvents, triggerStayEvent;
+   private void OnTriggerEnter(Collider other)
+   {
+      triggerEnterEvents.Invoke();
+   }
+
+   public void OnTriggerStay(Collider other)
+   {
+      triggerStayEvent.Invoke();
+   }
+}
