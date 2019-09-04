c43172564ee15faa377d752cfc583acf74b20402
Kaitlyn <kand8998@gmail.com>
2019-09-04 10:28:23 -0600

    Camera C#, Lights Events
    Script  was unity tutorial variant

---
+++ FallScripting2019/Assets/Scripts/CameraLookAt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAt : MonoBehaviour
+{
+    // Unity Tutorial Variant
+    public Transform target;
+    private void Update()
+    {
+        transform.LookAt(target);
+    }
+}
