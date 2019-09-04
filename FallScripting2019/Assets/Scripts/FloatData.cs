bb8f4303f10fa04eb6d5f4bf79a0770e5e8e4397
Kaitlyn <kand8998@gmail.com>
2019-09-04 14:22:54 -0600

    FloatData and Image Controller
    Health Bar example, and upper and lower limits;

--- FallScripting2019/Assets/Scripts/FloatData.cs
+++ FallScripting2019/Assets/Scripts/FloatData.cs
@@ -4,9 +4,36 @@
 public class FloatData : ScriptableObject
 {
     public float value = 1f;
+    public float maxValue = 1f;
     
     public void UpdateValue(float amount)
     {
         value += amount;
+    }
+
+    public void UpdateValueLimitZero(float amount)
+    {
+        if (value > 0)
+        {
+            UpdateValue(amount);
+        }
+        else
+        {
+            value = 0;
+        }
+    }
+
+    public void UpdateValueLimitZeroAndMaxValue(float amount)
+    {
+        if (value <= maxValue)
+        {
+            UpdateValue(amount);
+        }
+        else
+        {
+            value = maxValue;
+        }
+        UpdateValueLimitZero(amount);
+       
     }
 }
