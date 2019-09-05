efa9206ad5e3a8a407e5c8a13ca251ade5d0917c
Kaitlyn <kand8998@gmail.com>
2019-09-05 13:46:20 -0600

    Character Motion Scripts (Basic)
    Roughly based on 1610 script repository; will be applied to character in Marty's class.

---
+++ FallScripting2019/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MoveCharacter : MonoBehaviour
+{
+    public UnityEvent onGrounded, offGrounded;
+    public MoveBase characterMover;
+    private CharacterController controller;
+    
+   private void Start()
+   {
+       controller = GetComponent<CharacterController>();
+   }
+   
+   private void Update()
+    {
+        if (controller.isGrounded)
+        {
+            onGrounded.Invoke();
+        }
+        else
+        {
+            offGrounded.Invoke();
+        }
+
+        characterMover.Move(controller);
+    }
+}
