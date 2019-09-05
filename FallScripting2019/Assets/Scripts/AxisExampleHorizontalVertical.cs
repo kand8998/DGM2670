c41614d0a00614b64a1e699ff3b83b405c88417e
Kaitlyn <kand8998@gmail.com>
2019-09-05 13:33:44 -0600

    Axis Snap
    Unity tutorial variant and Rider Code fix, player is now fixed in position

---
+++ FallScripting2019/Assets/Scripts/AxisExampleHorizontalVertical.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class AxisExampleHorizontalVertical : MonoBehaviour
+{
+    // Unity Tutorial Variant
+    public float hRange;
+    public float vRange;
+    public void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+        var xPos = h * hRange;
+        var vPos = v * vRange;
+
+        transform.position = new Vector3(xPos, 0, vPos);
+    }
+}
