809a142541fb60bb27d2211190fd9edc99c3e264
Kaitlyn <kand8998@gmail.com>
2019-09-06 09:00:09 -0600

    Events for Game Scene Recreation
    Events needed for Gaming Essentials Scene

---
+++ FallScripting2019/Assets/Scripts/RatchAndClankMonoEvents.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class RatchAndClankMonoEvents : MonoBehaviour
+{
+    public UnityEvent startEvent, triggerEnterEvent, collisionEnterEvent;
+    private void Start()
+    {
+         startEvent.Invoke();   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        collisionEnterEvent.Invoke();
+    }
+}
