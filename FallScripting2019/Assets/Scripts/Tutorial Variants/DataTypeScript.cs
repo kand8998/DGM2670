84c760f40c3d973ea301e04b95efb05e876e06b0
Kaitlyn <kand8998@gmail.com>
2019-09-10 13:50:57 -0600

    DataTypes Vector 3 Script
    Unity Tutorial Variant

---
+++ FallScripting2019/Assets/Scripts/Tutorial Variants/DataTypeScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DataTypeScript : MonoBehaviour
+{
+    //Unity Tutorial Variant: Data Types
+    private void Start()
+    {
+        Vector3 currentPosition = transform.position;
+        currentPosition = new Vector3(0,2,0);
+    }
+}
