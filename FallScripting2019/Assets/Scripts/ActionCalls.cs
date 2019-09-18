d35507e9f76f8ee23f314245106d2f6931d4fece
Kaitlyn <kand8998@gmail.com>
2019-09-18 13:32:27 -0600

    Scale, Rotate, Translate
    Chunky Code Added

---
+++ FallScripting2019/Assets/Scripts/ActionCalls.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ActionCalls : MonoBehaviour
+{
+    public float speed = 1f, rotateSpeed = 30f, scaleSpeed = 0.2f;
+    private Vector3 location, rotations, scales;
+    private void Start() {
+        
+    }
+    private void Update() {
+        Move();
+        OnRotate();
+        OnScale();
+    }
+    public void Move() {
+        location.x = speed * Time.deltaTime;
+        transform.Translate(location);
+    }
+
+    public void OnRotate() {
+        rotations.y = rotateSpeed * Time.deltaTime;
+        transform.Rotate(rotations);
+    }
+
+    public void OnScale(){
+        scales.Set(scaleSpeed, scaleSpeed, scaleSpeed);
+        transform.localScale = scales;
+    }
+}
