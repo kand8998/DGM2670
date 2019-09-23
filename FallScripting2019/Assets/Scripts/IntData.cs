5a6e16e3b9ec462c1c1288b5fc644cb04c4de4a5
Kaitlyn <kand8998@gmail.com>
2019-09-23 14:54:32 -0600

    Character Controller Scripts

---
+++ FallScripting2019/Assets/Scripts/IntData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+    public int value = 0;
+    public int maxValue = 2;
+
+    public void UpdateToMaxValue()
+    {
+        value = maxValue;
+    }
+
+    public void UpdatetoNumber(int number)
+    {
+        value = number;
+    }
+}
