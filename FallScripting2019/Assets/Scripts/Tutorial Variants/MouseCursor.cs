ca08ce00b96bbc8befe134e1501a471e251d9eb4
Kaitlyn <kand8998@gmail.com>
2019-09-26 15:58:17 -0600

    Arrows and Cursor
    Will need to fix cursor script and add customization

---
+++ FallScripting2019/Assets/Scripts/Tutorial Variants/MouseCursor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+    // Tutorial Variant
+    private SpriteRenderer rend;
+    public Sprite handCursor;
+    public Sprite normalCursor;
+    private void Start() {
+        Cursor.visible = false;
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            rend.sprite = handCursor;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            rend.sprite = normalCursor;
+        }
+    }
+}
