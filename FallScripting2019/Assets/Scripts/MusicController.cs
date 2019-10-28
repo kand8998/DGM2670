02c81eee494b3bd4ff2bc791711dba694b193596
Kaitlyn <kand8998@gmail.com>
2019-10-28 17:11:23 -0600

    Music Controller

---
+++ FallScripting2019/Assets/Scripts/MusicController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+    
+    public void PlayMusic()
+    {
+        if (audioSource.isPlaying) return;
+        audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        audioSource.Stop();
+    }
+}
