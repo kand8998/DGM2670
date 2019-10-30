d5d96ef4b138937047a387a4972e681c48b90de4
Kaitlyn <kand8998@gmail.com>
2019-10-30 14:33:55 -0600

    Intensive Character Controller

---
+++ FallScripting2019/Assets/Scripts/Character/CharacterBase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public abstract class CharacterBase : ScriptableObject
+{
+    //public Transform characterTransform { get; set;}
+    public CharacterController Controller { get; set;}
+    
+    protected Vector3 Location;
+    protected Vector3 Orientation;
+    public float speed = 10f;
+    public float orientSpeed = 3f;
+    public abstract void Move();
+}
---
+++ FallScripting2019/Assets/Scripts/Character/CharacterPrime.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class CharacterPrime : CharacterBase
+{
+    public override void Move()
+    {
+        Location.Set(Input.GetAxis("Vertical")*speed,0,0);
+        Orientation.y = Input.GetAxis("Horizontal")*orientSpeed;
+        
+        Transform transform;
+        (transform = Controller.transform).Rotate(Orientation);
+        Controller.transform.TransformDirection(transform.position);
+        
+        Controller.Move(Location * Time.deltaTime);
+    }
+}
