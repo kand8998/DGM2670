5cb67917d0b0ca928210edb9cbcfdbb45d699ef3
Kaitlyn <kand8998@gmail.com>
2019-11-06 14:33:02 -0700

    script error missing;

--- FallScripting2019/Assets/Scripts/Interfaces/CharacterMoveJump.cs
+++ FallScripting2019/Assets/Scripts/Interfaces/CharacterMoveJump.cs
@@ -6,7 +6,7 @@ public class CharacterMoveJump : ACharacterBase, IMove
     public Vector3 location { get; set; }
     public override void Move(CharacterController controller)
     {
-        Location = Input.GetAxis("Horizontal") *moveSpeed;
+     
         Location.Set(0,-gravity,0);
         Controller.Move(location * Time.deltaTime);
     }
