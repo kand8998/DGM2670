ab57eaee1a56ab744d86e6501aaf043432d42f7d
Kaitlyn <kand8998@gmail.com>
2019-11-07 11:04:17 -0700

    Instancing Script Var 1

---
+++ FallScripting2019/Assets/Scripts/ActionPrac/DestoryObj.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class DestoryObj : MonoBehaviour
+{
+    private void Start()
+    {
+        Destroy(gameObject, 3.5f);
+    }
+}
---
+++ FallScripting2019/Assets/Scripts/ActionPrac/Instantiate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Instantiate : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public Transform launchTransform;
+    private GameAction instanceAction;
+
+    private void Start()
+    {
+        instanceAction.action += Instancing;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           instanceAction.Raise();
+        }
+    }
+
+    private void Instancing()
+    {
+        var projectileInstance = Instantiate(projectile, launchTransform.position,
+            launchTransform.rotation);
+        projectileInstance.AddForce(launchTransform.up * 350f);
+    }
+}
