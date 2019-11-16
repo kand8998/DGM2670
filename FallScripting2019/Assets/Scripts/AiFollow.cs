75af97705803624731bd8d2ff06533357efbd175
Kaitlyn <kand8998@gmail.com>
2019-11-15 17:33:33 -0700

    Fish Animation, and Nav Mesh

---
+++ FallScripting2019/Assets/Scripts/AiFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AiFollow : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player, destination;
+    private void Start()
+    {
+        destination = transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    private void Update()
+    {
+        agent.destination = destination.position;
+        destination = player;
+    }
+}
