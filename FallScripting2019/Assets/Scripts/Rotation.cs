339b13ea4e33b7252dbd7df12d7b65457ccbdf77
Kaitlyn <kand8998@gmail.com>
2019-11-19 16:54:02 -0700

    Script Rotation condition

--- FallScripting2019/Assets/Scripts/Rotation.cs
+++ FallScripting2019/Assets/Scripts/Rotation.cs
@@ -3,9 +3,20 @@
 public class Rotation : MonoBehaviour
 {
     public float spin = 35f;
+
     void Update()
     {
+
+        while (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Rotate();
+        }
+    }
+    
+    public void Rotate()
+    {
         transform.Rotate(0,0,spin*Time.deltaTime);
+
     }
 }
 
