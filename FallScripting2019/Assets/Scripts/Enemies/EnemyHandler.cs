128d81699412fe91cd0d1acd1b86a28768e8bdf4
Kaitlyn <kand8998@gmail.com>
2019-11-20 14:34:11 -0700

    Instantiate SO's

---
+++ FallScripting2019/Assets/Scripts/Enemies/EnemyHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class EnemyHandler : MonoBehaviour
+{
+    public FloatData healthBase;
+    private FloatData health;
+    void Start()
+    {
+        health = Instantiate(healthBase);
+        print(health.value);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        health.UpdateValue(0.1f);
+    }
+}
