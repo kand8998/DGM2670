40a6d5c8bc98a16349e89a048719497bdcacc379
Kaitlyn <kand8998@gmail.com>
2019-11-26 11:27:25 -0700

    Animation Action with a bool

---
+++ FallScripting2019/Assets/Scripts/AnimBoolAction.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AnimBoolAction : MonoBehaviour
+{
+    public Animator animator;
+    public GameAction boolAction;
+
+    private void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        boolAction.action += CanWalkBool;
+    }
+
+    private void CanWalkBool()
+    {
+        animator.SetBool("CanWalk", true);
+    }
+}
--- FallScripting2019/Assets/Scripts/ArrowEvents.cs
+++ FallScripting2019/Assets/Scripts/ArrowEvents.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 
 public class ArrowEvents : MonoBehaviour
 {
     public UnityEvent leftArrowEvent, rightArrowEvent;
+    
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
