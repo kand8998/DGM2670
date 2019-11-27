3b7573e9f4baf142c2c7f281ad55c31febf32c20
Kaitlyn <kand8998@gmail.com>
2019-11-27 09:26:53 -0700

    Rotation Automatic Action, Character Flip,

---
+++ FallScripting2019/Assets/Scripts/RotationAction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RotationAction : MonoBehaviour
+{
+    public float forwardSpin = 35f;
+    public float backSpin = -35f;
+
+    public GameAction forwardAction, backwardAction;
+
+    private void Start()
+    {
+        forwardAction.action += ForwardRotation;
+        backwardAction.action += BackwardRotation;
+    }
+    
+    private void ForwardRotation()
+    {
+        transform.Rotate(0,0,forwardSpin*Time.deltaTime);
+    }
+
+    private void BackwardRotation()
+    {
+        transform.Rotate(0,0,backSpin*Time.deltaTime);
+    }
+}
